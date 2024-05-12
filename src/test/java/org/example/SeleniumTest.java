a080aa42bbc376b2ab6a538afb2b5c73f5841820
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-12 22:06:23 +0200

    init

---
+++ src/test/java/org/example/SeleniumTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.List;
+import java.util.Set;
+
+public class SeleniumTest {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+    }
+}
