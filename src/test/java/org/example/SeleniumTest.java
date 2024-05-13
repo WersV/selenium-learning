71d57ada1430873bbd3960162261a018455e8453
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-13 20:37:31 +0200

    first test

--- src/test/java/org/example/SeleniumTest.java
+++ src/test/java/org/example/SeleniumTest.java
@@ -1,15 +1,22 @@
 package org.example;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
 
-import java.util.List;
-import java.util.Set;
 
 public class SeleniumTest {
     public static void main(String[] args) {
         WebDriver driver = new FirefoxDriver();
+    }
+
+    @Test
+    public void openGooglePage() {
+//        String path = "C:\\JavaProgramowanie\\drivers\\chromedriver.exe";
+//        System.setProperty("webdriver.chrome.driver", path);
+        System.setProperty("webdriver.gecko.driver", "C:\\JavaProgramowanie\\drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.google.com");
     }
 }
