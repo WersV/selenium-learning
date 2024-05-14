7bd430c08063b11d2b39c1f96b1e26c2d22cca6f
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-14 18:45:53 +0200

    even more examples of selenium capabilities

--- src/test/java/org/example/FirstRealTest.java
+++ src/test/java/org/example/FirstRealTest.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -23,5 +24,17 @@ public class FirstRealTest {
         searchField.sendKeys(Keys.ENTER);
         WebElement result = driver.findElement(By.xpath("//a[contains(@href,'selenium.dev')]//h3"));
         Assert.assertTrue(result.isDisplayed());
+    }
+
+    @Test
+    public void test2() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://seleniumdemo.com");
+        WebElement shopSpan = driver.findElement(By.xpath("//span[text()='Shop']"));
+        shopSpan.click();
+        WebElement courseTitle = driver.findElement(By.xpath("//h2[text()='Java Selenium WebDriver']"));
+        Assert.assertTrue(courseTitle.isDisplayed());
+        driver.quit();
     }
 }
