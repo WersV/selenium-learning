36e86f8ba382479d991480e257e95fd21fe14e83
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-25 21:04:50 +0200

    good beginning

---
+++ src/test/java/org/example/JsExecutor.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JsExecutor {
+
+    @Test
+    public void testExecutor() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+    }
+}
