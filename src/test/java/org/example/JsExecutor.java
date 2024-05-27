d8114f6afffd641edcd0a9efaa7f169c1f25a35b
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-27 16:52:56 +0200

    learn executor

--- src/test/java/org/example/JsExecutor.java
+++ src/test/java/org/example/JsExecutor.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,5 +15,11 @@ public class JsExecutor {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
+        WebElement nameInput = driver.findElement(By.cssSelector("#fname"));
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].setAttribute('value', 'Przemek')", nameInput);
+        driver.get("https://testeroprogramowania.github.io/selenium/");
+        driver.findElement(By.xpath("//a[text()='Załadowanie pliku']")).click();
+        driver.findElement(By.cssSelector("#myFile")).sendKeys("C:\\Users\\Przemek\\Downloads\\sample.txt");
     }
 }
