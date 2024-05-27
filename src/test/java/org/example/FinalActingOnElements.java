cac7c0afad8ef6456766a3770cc03fd000eb27bb
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-27 17:48:59 +0200

    add screnshoot named with date and time

--- src/test/java/org/example/FinalActingOnElements.java
+++ src/test/java/org/example/FinalActingOnElements.java
@@ -1,10 +1,41 @@
 package org.example;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class FinalActingOnElements {
     @Test
-    public void act() {
+    public void act() throws IOException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://testeroprogramowania.github.io/selenium/basics.html");
 
-    }
+        Actions actions = new Actions(driver);
+
+        //Elements localization
+        WebElement h1El = driver.findElement(By.cssSelector("h1"));
+        actions.moveToElement(h1El).perform();
+
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH-mm");
+        LocalTime currentTime = LocalTime.now();
+        String hourAndMin = currentTime.format(formatter);
+        TakesScreenshot screenshot = (TakesScreenshot) driver;
+        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+        FileUtils.copyFile(srcFile, new File("src/test/resources/screenshot_" + currentDate + "_" + hourAndMin +".png"));
+
+
+   }
 }
