e3429d073e8e905652c2b50ac256db11681c8657
Przemyslaw Botwina <przemek.botwina@o2.pl>
2024-05-30 13:48:48 +0200

    homework done

--- src/test/java/org/example/FinalActingOnElements.java
+++ src/test/java/org/example/FinalActingOnElements.java
@@ -5,6 +5,7 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 import java.io.File;
@@ -14,6 +15,7 @@ import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
+import java.util.Set;
 
 public class FinalActingOnElements {
     @Test
@@ -28,13 +30,61 @@ public class FinalActingOnElements {
         WebElement h1El = driver.findElement(By.cssSelector("h1"));
         actions.moveToElement(h1El).perform();
 
-        LocalDate currentDate = LocalDate.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH-mm");
-        LocalTime currentTime = LocalTime.now();
-        String hourAndMin = currentTime.format(formatter);
-        TakesScreenshot screenshot = (TakesScreenshot) driver;
-        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
-        FileUtils.copyFile(srcFile, new File("src/test/resources/screenshot_" + currentDate + "_" + hourAndMin +".png"));
+        driver.findElement(By.cssSelector("#clickOnMe")).click();
+        driver.switchTo().alert().accept();
+
+        driver.findElement(By.name("fname")).sendKeys("Przemek");
+        driver.findElement(By.xpath("//a[text()='Visit W3Schools.com!']")).click();
+        System.out.println(driver.getCurrentUrl());
+        driver.navigate().back();
+
+        WebElement selectEl = driver.findElement(By.cssSelector("select"));
+        Select cars = new Select(selectEl);
+        cars.selectByVisibleText("Saab");
+
+        driver.findElement(By.cssSelector("input[type='checkbox']")).click();
+
+        driver.findElement(By.cssSelector("input[type='radio'][value='male']")).click();
+
+        WebElement username = driver.findElement(By.cssSelector("input[name='username']"));
+        username.clear();
+        username.sendKeys("WersV");
+
+        WebElement password = driver.findElement(By.cssSelector("input[name='password']"));
+        password.clear();
+        password.sendKeys("SilneHasło1!");
+
+
+        driver.findElement(By.cssSelector("input[type='submit'][value='Submit']")).click();
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+        alert.accept();
+
+        driver.findElement(By.id("newPage")).click();
+        String currentWindow = driver.getWindowHandle();
+        Set<String> windowNames = driver.getWindowHandles();
+        for(String window : windowNames) {
+            if(!window.equals(currentWindow)) {
+                driver.switchTo().window(window);
+            }
+        }
+
+        driver.findElement(By.xpath("//div[text()='Zaakceptuj wszystko']")).click();
+        WebElement searchInput = driver.findElement(By.name("q"));
+        searchInput.sendKeys("Selenium");
+        searchInput.sendKeys(Keys.ENTER);
+
+        driver.switchTo().window(currentWindow);
+
+        driver.findElement(By.cssSelector("#container ul li ul li"));
+
+//        LocalDate currentDate = LocalDate.now();
+//        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH-mm");
+//        LocalTime currentTime = LocalTime.now();
+//        String hourAndMin = currentTime.format(formatter);
+//        TakesScreenshot screenshot = (TakesScreenshot) driver;
+//        File srcFile = screenshot.getScreenshotAs(OutputType.FILE);
+//        FileUtils.copyFile(srcFile, new File("src/test/resources/screenshot_" + currentDate + "_" + hourAndMin +".png"));
 
 
    }
